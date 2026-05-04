29948a1c717b48c55d5ec8f9b4596af2ecc92973
Juuso <juuso.sav@gmail.com>
2026-05-04 10:22:38 +0300

    Fix: Company entity properties

--- Domain/Entities/Company.cs
+++ Domain/Entities/Company.cs
@@ -5,8 +5,6 @@
         public Guid Id { get; set; }
         public string Name { get; set; } = "";
         public ICollection<Department> Departments { get; set; } = [];
-        public Guid DepartmentId { get; set; }
         public ICollection<Employee> Employees { get; set; } = [];
-        public Guid EmployeeId { get; set; }
     }
 }
---
+++ Migrations/20260504072210_FixCompanyEntityProperties.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PoissaHR.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixCompanyEntityProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DepartmentId",
+                table: "Companies");
+
+            migrationBuilder.DropColumn(
+                name: "EmployeeId",
+                table: "Companies");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "DepartmentId",
+                table: "Companies",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "EmployeeId",
+                table: "Companies",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+    }
+}
