4f21662e2024b18e3ed58a6b3141cab81a4a5634
Juuso <juuso.sav@gmail.com>
2026-05-08 09:16:08 +0300

    Add: EmployeeDto to project for added abstraction

--- Application/Services/EmployeeService/EmployeeService.cs
+++ Application/Services/EmployeeService/EmployeeService.cs
@@ -1,23 +1,39 @@
 ﻿using PoissaHR.Infrastructure.Data;
 using PoissaHR.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using PoissaHR.Shared.Dto;
 
 namespace PoissaHR.Application.Services.EmployeeService
 {
     public class EmployeeService : IEmployeeService
     {
         private readonly ApplicationDbContext _context;
-        
+
         public EmployeeService(ApplicationDbContext context)
         {
             _context = context;
         }
 
-        public async Task<IEnumerable<Employee>> GetAllEmployeesAsync()
+        public async Task<IEnumerable<EmployeeDto>> GetAllEmployeesAsync()
         {
             var employees = await _context.Employees
                 .Where(e => !e.IsDeleted)
                 .Include(e => e.Department)
+                .Select(e => new EmployeeDto
+                {
+                    Id = e.Id,
+                    FirstName = e.FirstName,
+                    LastName = e.LastName,
+                    Email = e.Email,
+                    Phone = e.Phone,
+                    CompanyId = e.CompanyId,
+                    DepartmentId = e.DepartmentId,
+                    BirthDate = e.BirthDate,
+                    CreatedAt = e.CreatedAt,
+                    IsDeleted = e.IsDeleted,
+                    DeletedAt = e.DeletedAt,
+                    Department = e.Department
+                })
                 .AsNoTracking()
                 .ToListAsync();
 
--- Application/Services/EmployeeService/IEmployeeService.cs
+++ Application/Services/EmployeeService/IEmployeeService.cs
@@ -1,11 +1,12 @@
 ﻿using PoissaHR.Infrastructure.Data;
 using PoissaHR.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using PoissaHR.Shared.Dto;
 
 namespace PoissaHR.Application.Services.EmployeeService
 {
     public interface IEmployeeService
     {
-        Task<IEnumerable<Employee>> GetAllEmployeesAsync();
+        Task<IEnumerable<EmployeeDto>> GetAllEmployeesAsync();
     }
 }
